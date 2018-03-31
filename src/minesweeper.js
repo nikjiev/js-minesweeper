95e88925897f2de85ffc23b0be828c7352d0708f
Nik Jiev <nikjiev@gmail.com>
2018-03-31 16:52:00 -0700

    Part 4 of minesweeper. Function to display number of bombs adjacent to the flipped tile

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -25,17 +25,49 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     board.push(row);
   }
   let numberOfBombsPlaced = 0;
-  /* Note: this while loop has the potential to place bombs on top of
-   already existing bombs. This will be fixed with control flow statements
-   in later lessons */
    while (numberOfBombsPlaced < numberOfBombs) {
      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-     board[randomRowIndex][randomColumnIndex] = 'B';
-     numberOfBombsPlaced++;
+     if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+       board[randomRowIndex][randomColumnIndex] = 'B';
+       numberOfBombsPlaced++;
+     }
+
   }
   return board;
 };
+
+// Display the number of bombs adjacenet to the flipped tile
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1,-1],
+    [-1,0],
+    [-1,1],
+    [0,-1],
+    [0,1],
+    [1,-1],
+    [1,0],
+    [1,1]
+  ];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows
+        && neighborColumnIndex >= && neighborColumnIndex < numberOfColumns) {
+          if (bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+            numberOfBombs++;
+          }
+        }
+
+  });
+  return numberOfBombs;
+};
+
+
 
 // Update printBoard() Function
 const printBoard = board => {
