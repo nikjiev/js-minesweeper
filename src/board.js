ca14607023e0ec8dd73caa016bafcfaa95dd92a5
Nik Jiev <nikjiev@gmail.com>
2018-04-08 16:14:11 -0700

    Part 6 (last) of minesweeper. Create game.js and board.js files. Erase minesweeper.js. Transpile code.

--- src/board.js
+++ src/board.js
@@ -1,26 +1,6 @@
-// Add Game class
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
 
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Game Over! Final Board:');
-      this._board.print();
-    } else if (!this._board.hasNonBombEmptySpaces()) {
-      console.log('Congratulations, you won!');
-    } else {
-      console.log('Current board:');
-      this._board.print();
-    }
-  }
-}
-
-// Add a Board Constructor
-class Board {
-
+export class Board {
+  // Add a Board Constructor
   constructor (numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -128,6 +108,3 @@ class Board {
     return board;
   }
 }
-
-const g = new Game(3, 3, 3);
-g.playMove(0, 0);
